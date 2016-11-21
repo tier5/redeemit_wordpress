37ece7af51d6e98947ea90a544e4ae04bf716490
root <github@tier5.us>
2016-11-21 20:48:10 +0530

    Fixed untracted files

--- wp-content/plugins/unyson/framework/extensions/builder/includes/option-types/builder/builder.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('FW')) die('Forbidden');
-
-require dirname( __FILE__ ) . '/extends/class-fw-option-type-builder.php';
-require dirname( __FILE__ ) . '/extends/class-fw-option-type-builder-item.php';
-require dirname( __FILE__ ) . '/includes/templates/init.php';
-require dirname( __FILE__ ) . '/includes/fullscreen.php';
--- wp-content/plugins/unyson/framework/extensions/shortcodes/shortcodes/table/static.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('FW')) die('Forbidden');
-
-fw_ext('shortcodes')->get_shortcode('button')->_enqueue_static();
-
-$shortcodes_extension = fw_ext('shortcodes');
-wp_enqueue_style(
-	'fw-shortcode-table',
-	$shortcodes_extension->get_declared_URI('/shortcodes/table/static/css/styles.css')
-);
